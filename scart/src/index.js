2f5459518e349b152b4c602fdb05d6b691d4433d
deepakkumar <deepak.kumar@sparxtechnologies.com>
2018-06-14 13:03:24 +0530

    2018-06-14 Implementing router

--- scart/src/index.js
+++ scart/src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+const rootReducer = combineReducers({
+
+});
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const appObj = <Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>;
+ReactDOM.render(appObj, document.getElementById('root'));
 registerServiceWorker();
