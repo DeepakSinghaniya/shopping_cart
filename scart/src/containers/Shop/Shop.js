65a9f93422d1ddb5992f1d2a6cc22dbbfa5272bf
deepakkumar <deepak.kumar@sparxtechnologies.com>
2018-06-18 11:02:55 +0530

    2018-06-18 update

--- scart/src/containers/Shop/Shop.js
+++ scart/src/containers/Shop/Shop.js
@@ -6,34 +6,40 @@ import bootstrap from '../../assets/scss/bootstrap.scss';
 import { chunk } from '../../utility/utility';
 import Product from '../../components/Product/Product';
 import httpInstance from '../../http/http';
-import withErrorHandler from '../../HOC/withHttpErrorHandler'; 
+import withErrorHandler from '../../HOC/withHttpErrorHandler';
 import Loader from '../../components/Loader/Loader';
+import classes from './Shop.scss';
 
 class Shop extends Component {
     componentDidMount() {
         this.props.initProducts(this.props.offset);
     }
-	loadMore = () => {
-		this.props.initProducts(this.props.offset);
-	}
+    loadMore = () => {
+        this.props.initProducts(this.props.offset);
+    }
     render() {
         const layoutedProducts = chunk(this.props.products, 3);
         return (
-            <Fragment>
-                <h1>Shop</h1>
-                {layoutedProducts.map((productsRow, index) => {
-                    return (
-                        <Row key={index} cssModule={bootstrap}>
-                            {productsRow.map(product => {
-                                return (<Col key={product.id} cssModule={bootstrap}><Product {...product} /></Col>);
-                            })}
-                        </Row>
-                    );
-                })}
-				
-				<Button color="success" onClick={this.loadMore} cssModule={bootstrap}>Load More</Button>
-				{<Loader show={this.props.loader} />}
-            </Fragment>
+            <Row cssModule={bootstrap}>
+                <Col cssModule={bootstrap} md={3}>
+                </Col>
+                <Col cssModule={bootstrap} md={9}>
+                    <h1>Shop</h1>
+                    {layoutedProducts.map((productsRow, index) => {
+                        return (
+                            <Row key={index} cssModule={bootstrap}>
+                                {productsRow.map(product => {
+                                    return (<Col md={4} key={product.id} cssModule={bootstrap}><Product {...product} /></Col>);
+                                })}
+                            </Row>
+                        );
+                    })}
+
+                    <Button className={classes['load-more-button']} color="success" onClick={this.loadMore} cssModule={bootstrap}>Load More</Button>
+                    <Loader show={this.props.loader} />
+                </Col>
+            </Row>
+
         );
     }
 }
@@ -41,7 +47,7 @@ const mapStoreToProps = store => {
     return {
         products: store.shop.products,
         loader: store.shop.loader,
-		offset: store.shop.offset
+        offset: store.shop.offset
     }
 }
 const mapDispatchToProps = dispatch => {
@@ -49,4 +55,4 @@ const mapDispatchToProps = dispatch => {
         initProducts: (offset) => dispatch(initialLoadProducts(offset))
     }
 }
-export default withErrorHandler(connect(mapStoreToProps, mapDispatchToProps)(Shop),httpInstance);
+export default withErrorHandler(connect(mapStoreToProps, mapDispatchToProps)(Shop), httpInstance);
